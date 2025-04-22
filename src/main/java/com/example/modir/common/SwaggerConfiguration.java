7df567961f56bb63f0312d105679fe7d6bf796f6
KWJ <wojun74@naver.com>
2025-04-22 16:03:01 +0900

    04.22 숨겨야 되는 정보들 secret으로 만들기

--- src/main/java/com/example/modir/common/SwaggerConfiguration.java
+++ src/main/java/com/example/modir/common/SwaggerConfiguration.java
@@ -2,6 +2,12 @@ package com.example.modir.common;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 
 @OpenAPIDefinition(
@@ -11,6 +17,17 @@ import io.swagger.v3.oas.annotations.info.Info;
         description = "모디랑 커뮤니티"
     )
 )
+@Configuration
 public class SwaggerConfiguration {
-
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("bearer-key",
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                ))
+                .addSecurityItem(new SecurityRequirement().addList("bearer-key"));
+    }
 }
