53b39f579a00f43c2778ba57f28e7d4079c3e938
황대찬 <ghkdeocks123@naver.com>
2025-05-13 15:00:47 +0900

    20250513 db 연결 시도1

--- src/main/java/com/example/modir/ModirApplication.java
+++ src/main/java/com/example/modir/ModirApplication.java
@@ -1,13 +1,29 @@
 package com.example.modir;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 public class ModirApplication {
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ModirApplication.class, args);
 	}
 
+	@PostConstruct
+	public void testDbConnection() {
+		try {
+			jdbcTemplate.queryForObject("SELECT 1", Integer.class);
+			System.out.println("✅✅ MariaDB 연결 성공 ✅✅");
+		} catch (Exception e) {
+			System.out.println("❌❌ MariaDB 연결 실패 ❌❌");
+			e.printStackTrace();
+		}
+	}
 }
