b0bf1003eb770cf5006263662b61835292cd1d8d
황대찬 <ghkdeocks123@naver.com>
2025-05-13 14:37:27 +0900

    20250513 db 연결 시도

--- src/main/java/com/example/modir/feed/like/model/FeedLikeRes.java
+++ src/main/java/com/example/modir/feed/like/model/FeedLikeRes.java
@@ -1,18 +1,17 @@
 package com.example.modir.feed.like.model;
 
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
-
-
 public class FeedLikeRes {
     private long feedId;
     @JsonIgnore
     private String uuid;
     private String username;
     private String title;
+    private int hits; // 추가: 조회수
+    private String createdAt; // 추가: 생성일
 }
